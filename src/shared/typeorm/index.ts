ad0ff836e94b0c2e7c08c4b95db607aeb9b0db5b
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-02-28 15:47:59 -0300

    Criação da conexão via typeorm com banco postgre em um container docker

---
+++ src/shared/typeorm/index.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const connection = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'apivendas',
+});
+
+connection
+  .initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch(err => {
+    console.error('Error during Data Source initialization', err);
+  });
